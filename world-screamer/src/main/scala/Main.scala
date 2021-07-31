005ad444e05ca39b1e1e33175d04fac26f37e34e
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-07-31 18:00:58 -0400

    Initialize Scala sandbox project

--- world-screamer/src/main/scala/Main.scala
+++ world-screamer/src/main/scala/Main.scala
@@ -1,5 +1,44 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+@main def hello: Unit =
+    Calc.main(new Array[String](0))
 
-def msg = "I was compiled by Scala 3. :)"
+// Try Creating Calc object from (https://docs.scala-lang.org/tutorials/scala-for-java-programmers.html)
+object Calc {
+    type Environment = String => Int
+
+    abstract class Tree
+    case class Derive(tree: Tree) extends Tree
+    case class Sum(l: Tree, r: Tree) extends Tree
+    case class Var(n: String) extends Tree
+    case class Const(v: Int) extends Tree
+
+    def main(args: Array[String]): Unit = {
+        val exp: Tree = Sum(Sum(Var("x"), Var("x")), Sum(Const(7), Var("y")))
+        val env: Environment = { case "x" => 5 case "y" => 7 }
+        println("Expression: " + exp)
+        println("Evaluation with x=5, y=7: " + eval(exp, env))
+        println("Derivative relative to x:\n " + derive(exp, "x"))
+        println("Derivative relative to y:\n " + derive(exp, "y"))
+        
+        // TODO Now I'll try having the derivation evaluated!
+        val deriveExp: Tree = Sum(Sum(Var("x"), Var("x")), Sum(Const(7), Var("y")))
+        val deriveEnv: Environment = { case "x" => 5 case "y" => 7 }
+        println("\n\n\n\n\n")
+        println("Now attempting to evaluate the derivation...")
+        println("Expression: " + exp)
+        println("Evaluation with x=5, y=7: " + eval(exp, env))
+        println("Derivative relative to x:\n " + derive(exp, "x"))
+        println("Derivative relative to y:\n " + derive(exp, "y"))
+    }
+    
+    def eval(t: Tree, env: Environment): Int = t match {
+        case Sum(l, r) => eval(l, env) + eval(r, env)
+        case Var(n)    => env(n)
+        case Const(v)  => v
+    }
+
+    def derive(t: Tree, v: String): Tree = t match {
+        case Sum(l, r) => Sum(derive(l, v), derive(r, v))
+        case Var(n) if (v == n) => Const(1)
+        case _ => Const(0)
+    }
+}
--- world-screamer/src/test/scala/Test1.scala
+++ world-screamer/src/test/scala/Test1.scala
@@ -1,5 +1,7 @@
 import org.junit.Test
 import org.junit.Assert.*
 
+val msg = "I was compiled by Scala 3. :)"
+
 class Test1:
-  @Test def t1(): Unit = assertEquals("I was compiled by Scala 3. :)", msg)
+    @Test def t1(): Unit = assertEquals("I was compiled by Scala 3. :)", msg)

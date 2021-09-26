e08251a00657081dc57b62823525c2394d4e85c5
Johnathon Robertson <johnathon.j.robertson@gmail.com>
2021-09-26 16:46:56 -0400

    Init

--- kotlin/src/main/kotlin/main.kt
+++ kotlin/src/main/kotlin/main.kt
@@ -1,3 +1,14 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+}
+
+fun setAndListExample() {
+
+    var uniqueParticipants = mutableSetOf<String>()
+    var participants = listOf("elePHPant", "Gopher", "Lenny", "Moby Dock", "Codey", "Gopher")
+
+    // Write your code below 🎤
+    uniqueParticipants.addAll(participants)
+    println("The talent show has ${uniqueParticipants.size} unique participants.")
+    uniqueParticipants.clear()
+    println(uniqueParticipants)
 }
